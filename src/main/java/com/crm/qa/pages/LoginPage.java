2370bc5fb511b9f5491b9afbf4dfb92e15a12546
Rajesh Markandan <Rajesh.Markandan@blackboard.com>
2018-07-09 14:05:22 +0530

    Page object model concept code

---
+++ src/main/java/com/crm/qa/pages/LoginPage.java
@@ -0,0 +1,51 @@
+package com.crm.qa.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.crm.qa.base.TestBase;
+
+public class LoginPage extends TestBase {
+	
+	
+	
+	@FindBy(name="username")
+	WebElement username;
+	
+	@FindBy(name="password")
+	WebElement password;
+	
+	@FindBy(xpath="//input[@type='submit']")
+	WebElement loginBtn;
+	
+	@FindBy(xpath="//button[contains(text(), 'Sign Up']")
+	WebElement signUpBtn;
+	
+	@FindBy(xpath="//img[@class='img-responsive']")
+	WebElement crmLogo;
+	
+	public LoginPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	public String validateLoginPageTitle() {
+		return driver.getTitle();
+	}
+	
+	public boolean validateCRMImage() {
+		return crmLogo.isDisplayed();
+	}
+	
+	public HomePage login(String un, String pwd) {
+		username.sendKeys(un);
+		password.sendKeys(pwd);
+		driver.switchTo().frame("intercom-borderless-frame");
+		driver.findElement(By.cssSelector("div[class='intercom-borderless-dismiss-button']")).click();
+		loginBtn.click();
+		
+		return new HomePage();
+	}
+
+}
